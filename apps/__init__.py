d90784a710683efef28c5daff49b866cb23ee35d
CuiDingke <15736980819@163.com>
2021-09-22 19:10:18 +0800

    '完成'

--- apps/__init__.py
+++ apps/__init__.py
@@ -13,7 +13,7 @@ config = {
 
 def create_app():
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
-    app.config.from_object(settings.ProductionConfig)
+    app.config.from_object(settings.DevelopmentConfig)
     # 注册蓝图
     app.register_blueprint(user_bp1)
     app.register_blueprint(article_bp1)
